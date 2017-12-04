9484fcd1e33a19c97bb3954f488ee21edaf43b82
Corey Alix <Corey.Alix@infor.com>
2017-12-04 15:08:32 -0500

    experimenting with nested properties (see draw.point)

--- components/index.tsx
+++ components/index.tsx
@@ -1,66 +1,93 @@
 import { PureComponent as Component, createElement as create } from 'react';
 import { render } from "react-dom";
-import { OpenLayers } from "./openlayers";
+import { OpenLayers, GeoJsonLayer } from "./openlayers";
+import { input } from "../common/common";
 
 export interface ToggleMapState {
     showmap: boolean;
     center: [number, number];
     zoom: number;
+    drawPointTest?: boolean;
+    portrait?: boolean;
 }
 
-export interface ToggleMapProps {
-    showmap: boolean;
-    center: [number, number];
-    zoom: number;
-    portrait?: boolean;
+export interface ToggleMapProps extends ToggleMapState {
 }
 
 export class ToggleMap extends Component<ToggleMapProps, ToggleMapState> {
 
     constructor(props: ToggleMapProps) {
         super(props);
+        props.drawPointTest = true;
         this.state = this.props;
     }
 
-    render() {
+    onMountMap() {
+
+    }
+
+    onMountLayerMap() {
+
+    }
+
+    render(): any {
 
         let setCenter = (v: [number, number], z: number) => {
             this.setState({ center: v, zoom: z });
         };
 
-        let orientation: "portrait"|"landscape" = this.props.portrait ? "portrait" : "landscape";
+        let orientation: "portrait" | "landscape" = this.state.portrait ? "portrait" : "landscape";
 
         return <div>
+            {input(this)}
             {this.state.showmap && <div>
                 <div>
                     <label>{this.state.center.map(v => v.toFixed(5)).join(",")} Z{this.state.zoom}</label>
                 </div>
                 <OpenLayers
-                    orientation = {orientation}
-                    controls={{ zoom: true, zoomToExtent: true }}
+                    orientation={orientation}
+                    controls={{
+                        zoom: true,
+                        zoomToExtent: true,
+                    }}
                     setCenter={setCenter}
                     center={this.state.center}
-                    zoom={this.state.zoom} />
+                    zoom={this.state.zoom}
+                    osm={false}>
+                    <GeoJsonLayer url="http://openlayers.org/en/master/examples/data/geojson/countries.geojson" />
+                </OpenLayers>
                 <OpenLayers
-                    orientation = {orientation}
-                    controls={{ fullScreen: true }}
-                    bingImagerySet="Aerial"
-                    labels={true}
-                    setCenter={setCenter}
-                    center={this.state.center}
-                    zoom={this.state.zoom} />
-                <OpenLayers
-                    orientation = {orientation}
+                    orientation={orientation}
                     controls={{ mousePosition: true }}
                     bingImagerySet="AerialWithLabels"
                     labels={true}
                     setCenter={setCenter}
                     center={this.state.center}
-                    zoom={this.state.zoom} />
+                    zoom={this.state.zoom}
+                    geoJsonUrl="http://openlayers.org/en/master/examples/data/geojson/countries.geojson" />
                 <OpenLayers
-                    orientation = {orientation}
-                    controls={{ scaleLine: true }}
-                    bingImagerySet="Road"
+                    orientation={orientation}
+                    controls={{ fullScreen: true }}
+                    bingImagerySet="Aerial"
+                    labels={true}
+                    setCenter={setCenter}
+                    center={this.state.center}
+                    zoom={this.state.zoom}
+                    geoJsonUrl="https://gist.githubusercontent.com/ca0v/78c82dbcb184d52f784a9aa11a452272/raw/5929e9469e02363665017202394aabba906845ae/trip.geojson" />
+                <OpenLayers
+                    orientation={orientation}
+                    allowPan={false}
+                    allowZoom={false}
+                    controls={{
+                        scaleLine: true,
+                        draw: {
+                            circle: true,
+                            line: true,
+                            point: this.state.drawPointTest,
+                            polygon: true,
+                        }
+                    }}
+                    osm={true}
                     setCenter={setCenter}
                     center={this.state.center}
                     zoom={this.state.zoom} />
--- components/openlayers.tsx
+++ components/openlayers.tsx
@@ -1,4 +1,10 @@
-import { PureComponent as Component, createElement as create } from 'react';
+import {
+    PureComponent as Component,
+    createElement as create,
+    Children,
+    ReactElement
+} from 'react';
+
 import { debounce } from "../common/common";
 import * as ol from "openlayers";
 
@@ -8,11 +14,20 @@ interface OpenLayersProps {
     setCenter?: (v: [number, number], z: number) => void;
     title?: string;
     bingImagerySet?: string;
+    osm?: boolean;
     labels?: boolean;
     center: [number, number];
     zoom: number;
     orientation?: Orientations;
+    allowPan?: boolean;
+    allowZoom?: boolean;
     controls?: {
+        draw?: {
+            point?: boolean;
+            circle?: boolean;
+            line?: boolean;
+            polygon?: boolean;
+        };
         zoom?: boolean;
         zoomSlider?: boolean;
         zoomToExtent?: boolean;
@@ -21,6 +36,7 @@ interface OpenLayersProps {
         rotate?: boolean;
         scaleLine?: boolean;
     };
+    geoJsonUrl?: string;
 }
 
 interface OpenLayersState {
@@ -38,10 +54,40 @@ export class OpenLayers extends Component<OpenLayersProps, OpenLayersState> {
     }
 
     render() {
+        let map = this.state.map;        
+        let DrawControls = () => <div />;
+        if (this.props.controls) {
+            if (this.props.controls.draw) {
+                let draw = this.props.controls.draw;
+                DrawControls = () => <div>
+                    {draw.point && <button onClick={() => map && this.draw(map, "Point")}>Point</button>}
+                    {draw.circle && <button onClick={() => map && this.draw(map, "Circle")}>Circle</button>}
+                    {draw.line && <button onClick={() => map && this.draw(map, "LineString")}>Line</button>}
+                    {draw.polygon && <button onClick={() => map && this.draw(map, "Polygon")}>Polygon</button>}
+                </div>
+            }
+        }
+
         return <div className='maplet'>
             {this.props.title && <label>{this.props.title}</label>}
             <div className={`map ${this.props.orientation || ''}`} ref={v => this.setState({ target: v })}></div>
+            <DrawControls />
+            {this.props.children}
         </div>;
+    }
+
+    /**
+     * BAD PRACTICE - Makes map available to child components
+     */
+    parent() {
+        return this;
+    }
+
+    /**
+     * BAD PRACTICE - Makes map available to child components
+     */
+    public static childContextTypes = {
+        parent: OpenLayers
     }
 
     componentDidMount() {
@@ -51,9 +97,67 @@ export class OpenLayers extends Component<OpenLayersProps, OpenLayersState> {
                 zoom: this.props.zoom
             }),
             controls: [],
+            interactions: [],
         });
 
+        if (this.props.allowPan !== false) {
+            map.addInteraction(new ol.interaction.DragPan());
+            map.addInteraction(new ol.interaction.KeyboardPan());
+        }
+        if (this.props.allowZoom !== false) {
+            map.addInteraction(new ol.interaction.MouseWheelZoom());
+            map.addInteraction(new ol.interaction.DragZoom());
+        }
+
+        map.on("moveend", debounce(() => {
+            this.props.setCenter && this.props.setCenter(
+                ol.proj.toLonLat(map.getView().getCenter()),
+                map.getView().getZoom());
+        }, 50));
+
+        if (this.props.bingImagerySet) {
+            map.addLayer(new ol.layer.Tile({
+                source: new ol.source.BingMaps({
+                    key: 'AuPHWkNxvxVAL_8Z4G8Pcq_eOKGm5eITH_cJMNAyYoIC1S_29_HhE893YrUUbIGl',
+                    imagerySet: this.props.bingImagerySet
+                })
+            }));
+        } else if (this.props.osm !== false) {
+            map.addLayer(new ol.layer.Tile({ source: new ol.source.OSM() }));
+        }
+
+        this.setState((prev, prop) => ({
+            map: map,
+        }));
+
+        // one alternative is to not use child components but properties...
+        this.props.geoJsonUrl && GeoJsonLayer.addGeoJsonLayer(map, this.props.geoJsonUrl);
+
+        // another is to use child components and pass parent to a handler..
+        Children.forEach(this.props.children, c => {
+            c;
+            if (typeof c === "string") return;
+            if (typeof c === "number") return;
+            let child: any = c;
+            if (child.type === GeoJsonLayer) {
+                GeoJsonLayer.addGeoJsonLayer(map, c.props.url);
+            }
+        });
+
+        // I would prefer that the child trigger a cascading event that the parent could receive
+        // and process
+
         if (this.props.controls) {
+
+            if (this.props.controls.draw) {
+                let draw = this.props.controls.draw;
+                if (draw.point || draw.circle || draw.line || draw.polygon) {
+                    draw.polygon && this.addDraw(map, "Polygon");
+                    draw.circle && this.addDraw(map, "Circle");
+                    draw.line && this.addDraw(map, "LineString");
+                    draw.point && this.addDraw(map, "Point");
+                }
+            }
 
             if (this.props.controls.zoom) {
                 map.addControl(new ol.control.Zoom({
@@ -92,27 +196,38 @@ export class OpenLayers extends Component<OpenLayersProps, OpenLayersState> {
             }
         }
 
-        map.on("moveend", debounce(() => {
-            this.props.setCenter && this.props.setCenter(
-                ol.proj.toLonLat(map.getView().getCenter()),
-                map.getView().getZoom());
-        }, 50));
+    }
 
-        if (this.props.bingImagerySet) {
-            map.addLayer(new ol.layer.Tile({
-                source: new ol.source.BingMaps({
-                    key: 'AuPHWkNxvxVAL_8Z4G8Pcq_eOKGm5eITH_cJMNAyYoIC1S_29_HhE893YrUUbIGl',
-                    imagerySet: this.props.bingImagerySet
-                })
-            }));
-        } else {
-            map.addLayer(new ol.layer.Tile({ source: new ol.source.OSM() }));
-        }
+    draw(map: ol.Map, type: ol.geom.GeometryType) {
+        map.getInteractions().getArray().forEach(interaction => {
+            if (interaction instanceof ol.interaction.Draw) {
+                if (interaction.get("type") === type) {
+                    // toggle
+                    interaction.setActive(!interaction.getActive());
+                } else {
+                    // disable
+                    interaction.setActive(false);
+                }
+            }
+        });
+    }
 
-        this.setState((prev, prop) => ({
-            map: map,
-        }));
+    addDraw(map: ol.Map, type: ol.geom.GeometryType) {
+        let source = new ol.source.Vector();
+        let layer = new ol.layer.Vector({
+            source: source
+        });
 
+        let interaction = new ol.interaction.Draw({
+            source: source,
+            type: type
+        });
+        interaction.set("type", type);
+
+        interaction.setActive(false);
+        map.addInteraction(interaction);
+
+        map.addLayer(layer);
     }
 
     componentDidUpdate(prevProp: OpenLayersProps, prevState: OpenLayersState) {
@@ -129,5 +244,60 @@ export class OpenLayers extends Component<OpenLayersProps, OpenLayersState> {
     }
 
     componentWillUnmount() {
+        // will this force the map to be destroyed?
+        this.setState({ target: null });
+    }
+
+}
+
+export interface GeoJsonLayerProps {
+    url: string;
+}
+
+export interface GeoJsonLayerState {
+}
+
+/**
+ * This is not the react way I don't think...but what *is* the react way?
+ * Need a shared state between parent/children so they all get access to "map"
+ */
+export class GeoJsonLayer extends Component<GeoJsonLayerProps, GeoJsonLayerState> {
+    render() {
+        return <div>Here I am</div>;
+    }
+
+    static addGeoJsonLayer(map: ol.Map, url: string) {
+        // how to access parent map to add layer...should be a inferred "props" of sorts
+        let vector = new ol.layer.Vector({
+            source: new ol.source.Vector({
+                url: url,
+                format: new ol.format.GeoJSON()
+            })
+        });
+
+        map.addLayer(vector);
+    }
+
+    componentDidMount() {
+        // how to access parent map to add layer...should be a inferred "props" of sorts
+        let vector = new ol.layer.Vector({
+            source: new ol.source.Vector({
+                url: this.props.url,
+                format: new ol.format.GeoJSON()
+            })
+        });
+
+        console.log("context:", this.context);
+        let parent: OpenLayers = this.context.parent;
+        if (parent && parent.state.map) {
+            parent.state.map.getLayers().insertAt(0, vector);
+        }
+    }
+
+    /**
+     * BAD PRACTICE - Makes map available to this component
+     */
+    public static contextTypes = {
+        parent: OpenLayers
     }
 }

01a5a68a63c071c2a664b7b52c0d58e000dd8a09
ca0v <coreyalix@hotmail.com>
2020-08-15 02:07:51 -0400

    adds browser testing

---
+++ built/test/index.js
@@ -0,0 +1,12 @@
+define("index", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.run = void 0;
+    function run() {
+        describe("describe", () => {
+            it("it", () => { });
+        });
+    }
+    exports.run = run;
+});
+//# sourceMappingURL=index.js.map
---
+++ test/index.ts
@@ -0,0 +1,8 @@
+declare var describe: any;
+declare var it: any;
+
+export function run() {
+  describe("describe", () => {
+    it("it", () => {});
+  });
+}
